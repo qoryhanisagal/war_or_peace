da8d33e8a9c07f2b46c32b8c311610e45a087928
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-14 16:48:36 +0200

    Add test for Card class to chek creation and attributes

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,18 +1,32 @@
-require 'rspec'
-require './lib/card'
+# Require the Card class
+# I prefer `require_relative` as require `rspec` cause too many error when I attempted this last week.
+require_relative '../lib/card'
 
+# Begin describing the Card class
 RSpec.describe Card do
-  it "exists" do
-    card = Card.new(:diamond, 'Queen', 12)
+  # Test to check if a card can be successfully created
+  it 'can be created with a rank, suit and value' do
+    # Create a new Card object
+    card = Card.new('Ace', :spade, 14)
 
-    expect(card).to be_an_instance_of(Card)
+    # Expect the card to have a rank of 'Ace'
+    expect(card.rank).to eq('Ace')
+    # Expect the card to have a suit of :spade
+    expect(card.suit). to eq(:spade)
+    # Expect the card to have a value of 14
+    expect(card.value). to eq(14)
   end
 
-  it "has readable attributes" do
-    card = Card.new(:diamond, 'Queen', 12)
+  # Test to check if the card has readable attributes are first test failed
+  it 'has readable attributes' do
+    # Create a new Card object with rank 'Queen', suit diamond, and value 12
+    card = Card.new('Queen', :diamond, 12)
 
+    # Expect the card to have a suit of :diamond
     expect(card.suit).to eq(:diamond)
-    expect(card.value).to eq('Queen')
-    expect(card.rank).to eq(12)
+    # Expect the card to have a rank of 'Queen'
+    expect(card.rank).to eq('Queen')
+    # Expect the card to have a value of 12
+    expect(card.value). to eq(12)
   end
 end
