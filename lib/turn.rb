ebb9f4fa3f122bc93b8c1164d9df376d2bb4335f
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-16 14:55:00 +0200

    Add initialize method to Turn class to set up players and spoils_of_war

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,8 @@ class Turn
 end
 
 # Initialize a turn with two players and an empty spoils_of_war array
+def initialize(player1, player2)
+  @player1 = player1              # The first player in the turn
+  @player2 = player2              # The second player in the turn
+  @spoils_of_war = []             # Cards that the winner will collect during this turn
+end
