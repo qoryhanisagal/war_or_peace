cc3cc395a2af6a9b642773d95d517253b16a9815
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-16 12:24:54 +0200

    Refactor deck_spec.rb to comply with RuboCop block length rule

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,56 +4,58 @@ require_relative '../lib/card'
 
 # Begin describing the Deck class
 RSpec.describe Deck do
- # Test that a deck can be created with an array of Card objects
-  it 'can be created with cards' do
-    card1 = Card.new('Ace', :spade, 14) # Create a card
-    card2 = Card.new('King', :heart, 13) # Create another card
-    deck = Deck.new([card1, card2]) # Create a deck with the cards
+  context 'when dealing with a new deck' do
+    it 'can be created with cards' do
+      # Create two card objects
+      card1 = Card.new('Ace', :spade, 14)   # Create a card
+      card2 = Card.new('King', :heart, 13)  # Create another card
+      deck = Deck.new([card1, card2])       # Create a deck with the cards
 
-    # Expect the deck to contain the cards
-    expect(deck.cards).to eq([card1, card2])
+      # Expect the deck to contain the cards
+      expect(deck.cards).to eq([card1, card2])
+    end
+
+    it 'can count the number of cards in the deck' do
+      card1 = Card.new('Ace', :spade, 14)
+      card2 = Card.new('King', :heart, 13)
+      deck = Deck.new([card1, card2])
+
+      # Expect the count to return 2 initially
+      expect(deck.count).to eq(2)
+
+      # Deal a card and expect the count to return 1
+      deck.deal
+      expect(deck.count).to eq(1)
+    end
   end
 
-# Test that the deck can shuffle the cards
-it 'can shuffle the deck' do
-  card1 = Card.new('Ace', :spade, 14)
-  card2 = Card.new('King', :heart, 13)
-  deck = Deck.new([card1, card2])
+  context 'when performing operations on the deck' do
+    it 'can shuffle the deck' do
+      card1 = Card.new('Ace', :spade, 14)
+      card2 = Card.new('King', :heart, 13)
+      deck = Deck.new([card1, card2])
 
-# Call the shuffle method
-    deck.shuffle
+      # Call the shuffle method
+      deck.shuffle
 
-# Since the shuffle is random, I can't check excat order, but the deck should still contain both cards
-    expect(deck.cards) .to contain_exactly(card1, cards2)
-  end
+      # Since the shuffle is random, I can't check the exact order.
+      # But the deck should still contain both cards.
+      expect(deck.cards).to contain_exactly(card1, card2)
+    end
 
-# Test that a card can be dealt from the deck
-  it 'can deal a card from the deck' do
-    card1 = Card.new('Ace', :spade, 14)
-    card2 = Card.new('King', :heart, 13)
-    deck = Deck.new([card1, card2])
+    it 'can deal a card from the deck' do
+      card1 = Card.new('Ace', :spade, 14)
+      card2 = Card.new('King', :heart, 13)
+      deck = Deck.new([card1, card2])
 
-    # Deal a card from the deck
-  dealt_card = deck.deal
+      # Deal a card from the deck
+      dealt_card = deck.deal
 
-    # Expect the dealt card to be the last one added (King of Hearts)
-  expect(dealt_card). to eq(card2)  
+      # Expect the dealt card to be the last one added (King of Hearts)
+      expect(dealt_card).to eq(card2)
 
-    # Expect the decl to now only contain the first card
-  expect(deck.cards). to eq([card1])
-end
-
-# Test that the deck knows how many cards are left
-it 'can count the number of cards in the deck' do
-  card1 = Card.new('Ace', :spade, 14)
-  card2 = Card.new('King', :heart, 13)
-  deck = Deck.new([card1, card2])
-
-# Expect the count to return 2 initially 
-expect(deck.count) . to eq (2)
-
-# Deal a card and expect the count to return 1
-deck.deal
-expect(deck.count) .to eq(1)
+      # Expect the deck to now only contain the first card
+      expect(deck.cards).to eq([card1])
+    end
   end
 end
