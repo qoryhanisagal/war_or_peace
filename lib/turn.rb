3f4a8646768af56c629574c11b87be7cca3cd1cf
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-16 12:52:17 +0200

    Add Turn class definition and attr_reader for player1, player2, and spoils_of_war

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,2 +1,5 @@
 # A class to represent a turn in a game
 class Turn
+  # Use attr_reader to automatically create getter methods for player1, player2, and spoils_of_war
+  attr_reader :player1, :player2, :spoils_of_war
+end
