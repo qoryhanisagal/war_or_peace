4423d9c85d9bfb1aa039a7aaf1778bb4535beef6
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-16 14:56:32 +0200

    Add type method to determine turn type (basic, war, mutually assured destruction)

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,19 @@ def initialize(player1, player2)
   @player2 = player2              # The second player in the turn
   @spoils_of_war = []             # Cards that the winner will collect during this turn
 end
+
+  # Determine the type of turn based on the rank of the players' cards
+  # There are three types of turns: basic, war, and mutually_assured_destruction
+  def type
+    # Basic turn: Players have different ranks for the first card
+    if player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    # War turn: Players have the same rank for the first card but different ranks for the third card
+    elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) &&
+          player1.deck.rank_of_card_at(2) != player2.deck.rank_of_card_at(2)
+      :war
+    # Mutually Assured Destruction: Players have the same rank for the first and third cards
+    else
+      :mutually_assured_destruction
+    end
+  end
